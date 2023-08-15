a9940747844f27cdee7e6e635eed0d6a65edeb1d
abhink <kabhinav86@gmail.com>
2023-08-15 11:10:00 +0200

    remove spurious statements from main.go

--- main.go
+++ main.go
@@ -29,17 +29,13 @@ func readStdin() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	var lines []string
 
-	fmt.Println("Enter multiline text (Ctrl+D or Ctrl+Z on Windows to end):")
-
 	for scanner.Scan() {
 		line := scanner.Text()
 		lines = append(lines, line)
 	}
-
 	if scanner.Err() != nil {
 		fmt.Println("Error reading:", scanner.Err())
 		return ""
 	}
-
 	return strings.Join(lines, "\n")
 }
